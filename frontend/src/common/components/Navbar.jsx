4f44d20308049df273d67c5b7b23d42faf4b01a7
msourov <sourov1666@gmail.com>
2025-11-30 20:47:38 +0600

    modify: name

--- frontend/src/common/components/Navbar.jsx
+++ frontend/src/common/components/Navbar.jsx
@@ -17,7 +17,7 @@ const Navbar = () => {
     <nav className="navbar">
       <div className="navbar-container">
         <Link to="/" className="navbar-brand">
-          💬 CommentHub
+          ThreadSphere
         </Link>
 
         <div className="navbar-menu">
